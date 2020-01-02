8ee692b3a78a7bd7cd4a13282ac7cc14a93fd153
Ryan Harings <ryanharings@hotmail.com>
2020-01-02 10:36:01 -0700

    removed auth use for now

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ryanHarings/emotion-app-be
 go 1.12
 
 require (
+	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"database/sql"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,11 +12,33 @@ import (
 	"strconv"
 	"time"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	_ "github.com/lib/pq"
 )
+
+// Response - not sure what this does
+type Response struct {
+	Message string `json:"message"`
+}
+
+// Jwks - not sure what this does
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+// JSONWebKeys - not sure what this does
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
 
 func repeatHandler(r int) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -87,9 +111,36 @@ func dbFunc(db *sql.DB) gin.HandlerFunc {
 	}
 }
 
-func main() {
-	port := os.Getenv("PORT")
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
 
+func main() {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			aud := os.Getenv("AUTH0_API_AUDIENCE")
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAudience {
+				return token, errors.New("invalid audience")
+			}
+			// verify iss claim
+			iss := os.Getenv("AUTH0_DOMAIN")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("invalid issuer")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				log.Fatalf("could not get cert: %+v", err)
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+	jwtMiddleWare = jwtMiddleware
+
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
@@ -137,6 +188,7 @@ func main() {
 	router.GET("/db", dbFunc(db))
 
 	router.GET("/emotions", sendEmotions())
+	// router.GET("/emotions", authMiddleware(), sendEmotions())
 
 	// router.OPTIONS("/emotions", preflight)
 
@@ -148,3 +200,47 @@ func main() {
 // 	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 // 	c.JSON(http.StatusOK, struct{}{})
 // }
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	x5c := jwks.Keys[0].X5c
+	for k, v := range x5c {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		return cert, errors.New("unable to find appropriate key")
+	}
+
+	return cert, nil
+}
+
+// authMiddleware intercepts the requests, and check for a valid jwt token
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the client secret key
+		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
+		if err != nil {
+			// Token not found
+			fmt.Println(err)
+			c.Abort()
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Unauthorized"))
+			return
+		}
+	}
+}
